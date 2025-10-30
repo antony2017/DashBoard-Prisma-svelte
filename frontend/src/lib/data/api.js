c89c5c1f61677a3cb5fe91875d6ae50c66285b81
erickBMO2017 <erick.araque.bmo@gmail.com>
2025-10-30 13:51:37 -0400

    Actualizacion

--- frontend/src/lib/data/api.js
+++ frontend/src/lib/data/api.js
@@ -1,7 +1,8 @@
 // src/lib/data/api.js
 
 //const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL || "dashboard-prisma-backend.up.railway.app/api";
 /**
  * Consulta el endpoint /multiple-requests para obtener la lista de sucursales por servidor.
  * @returns {Promise<Array<Object>>} Los resultados brutos de cada servidor.
